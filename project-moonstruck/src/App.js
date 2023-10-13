e669b105d43dc79bdd8a79c72ddc9b32101c78d3
Mjacke <marshall.jacob16@icloud.com>
2023-10-13 16:07:52 -0600

    Got the page to render. Need to format the navbar.

--- project-moonstruck/src/App.js
+++ project-moonstruck/src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Home from './components/navbarcomps/Home'
 import Reviews from './components/navbarcomps/Reviews';
+import Categories from './components/navbarcomps/Categories';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Home');
 
   // Here we are pulling the navbar components and rendering those
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home/>;
+    if (currentPage === 'Categories') {
+      return <Categories/>;
     }
     if (currentPage === 'Reviews') {
       return <Reviews/>;
     }
+    return <Home/>
     // Eventually build a flyout for different categories.
     // Probably something for "games, music, tech, hardware, etc""    "
   };
@@ -23,7 +26,7 @@ function App() {
   return (
     <>
       <div className="mainbody">
-        <navTabs currentPage={currentPage} handlePageChange={handlePageChange}/>
+        <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
         {renderPage()}
       </div>
     </>
--- project-moonstruck/src/components/Navbar.js
+++ project-moonstruck/src/components/Navbar.js
@@ -2,8 +2,33 @@ import React from "react";
 
 function Navbar ({ currentPage, handlePageChange }) {
     return (
-        
-    )
+        <ul className="nav">
+        <h3 id="nav-title">
+          Moonstruck Martian
+        </h3>
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+  
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#reviews"
+            onClick={() => handlePageChange('Reviews')}
+  
+            className={currentPage === 'Reviews' ? 'nav-link active' : 'nav-link'}
+          >
+            Reviews
+          </a>
+        </li>
+        </ul>
+  
+    );
 }
 
 export default Navbar;
