c72e8a30c122df3a6d7e04bc54541036bfbd2edc
Mjacke <marshall.jacob16@icloud.com>
2023-12-11 16:04:55 -0700

    adjust navbar to eventually be a collapsing side nav. git push

--- project-moonstruck/src/components/Navbar.js
+++ project-moonstruck/src/components/Navbar.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Navbar ({ currentPage, handlePageChange }) {
-    return (
-        <ul className="nav">
-        <h3 id="nav-title">
-          Moonstruck Martian
-        </h3>
-        <li className="nav-item">
-          <a
-            href="#home"
-            onClick={() => handlePageChange('Home')}
-  
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-          >
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a
-            href="#reviews"
-            onClick={() => handlePageChange('Reviews')}
-  
-            className={currentPage === 'Reviews' ? 'nav-link active' : 'nav-link'}
-          >
-            Reviews
-          </a>
-        </li>
+const Navbar = ({ currentPage, handlePageChange }) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" onClick={toggleCollapse}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className={`collapse navbar-collapse ${collapsed ? '' : 'show'}`}>
+        <ul className="navbar-nav mr-auto">
+a          <li className="nav-item">
+            <a
+              href="#home"
+              onClick={() => {
+                handlePageChange('Home');
+                setCollapsed(true); // Collapse sidebar when a link is clicked
+              }}
+              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+            >
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#reviews"
+              onClick={() => {
+                handlePageChange('Reviews');
+                setCollapsed(true); // Collapse sidebar when a link is clicked
+              }}
+              className={currentPage === 'Reviews' ? 'nav-link active' : 'nav-link'}
+            >
+              Reviews
+            </a>
+          </li>
         </ul>
-  
-    );
-}
+      </div>
+
+    </nav>
+  );
+};
 
 export default Navbar;
